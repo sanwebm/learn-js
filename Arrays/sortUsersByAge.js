f2d8adc90ac8f5c8cff066b4d62e41c4442be69f
sanwebm <sanwebm@yandex.ru>
2019-10-08 15:20:54 +0300

    Sort users by age

---
+++ Arrays/sortUsersByAge.js
@@ -0,0 +1,18 @@
+let vasya = { name: "Вася", age: 25 };
+let petya = { name: "Петя", age: 30 };
+let masha = { name: "Маша", age: 28 };
+
+let arr = [ vasya, petya, masha ];
+
+sortByAge(arr);
+
+function sortByAge(arr){
+    arr.sort( (a,b) => {
+        return a.age - b.age
+    } )
+}
+
+// теперь: [vasya, masha, petya]
+console.log(arr[0].name); // Вася
+console.log(arr[1].name); // Маша
+console.log(arr[2].name); // Петя
